1b1c10d56e2dcaf578229de11812e24b3034606b
mgumbar <mgumbar@gmail.com>
2019-11-25 20:26:09 +0100

    FIRST COMMIT

---
+++ src/main/java/com/journaldev/bootifulmongo/BootifulMongoApplication.java
@@ -0,0 +1,13 @@
+package com.journaldev.bootifulmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifulMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootifulMongoApplication.class, args);
+	}
+
+}
